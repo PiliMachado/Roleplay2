42df55e0a91bf527c0adf1fbb8ed8f8e8ca2383f
SanFerri <santiago.ferraro@correo.ucu.edu.uy>
2022-05-09 20:09:52 -0300

    Fix en ICharacter y clases ahora implementan ICharacter

--- src/Library/Interfaces/ICharacter.cs
+++ src/Library/Interfaces/ICharacter.cs
@@ -7,12 +7,12 @@ namespace RoleplayGame
     /// </summary>
     public interface ICharacter
     {
-        int Health {get; set;}
-        int Name {get; set;}
+        int Health {get; }
+        string Name {get; set;}
         int AttackValue {get; }
         int DefenseValue {get; }
 
-        void Attack(ICharacter character);
+        void ReceiveAttack(int power);
         void Cure();
 
 
